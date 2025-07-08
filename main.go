8a2c121860df3e5e1538e43864e6d4229ce9285f
Craig Glendenning <craig@ziprecruiter.com>
2025-07-07 20:25:58 -0700

    add debugging so I understand broken API keys

--- main.go
+++ main.go
@@ -4,23 +4,39 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/stripe/stripe-go/v78"
-	"github.com/stripe/stripe-go/v78/checkout/session"
-	"gopkg.in/gomail.v2"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/stripe/stripe-go/v78"
+	"github.com/stripe/stripe-go/v78/checkout/session"
+	"gopkg.in/gomail.v2"
 )
 
 func main() {
 	// Load environment variables from .env file
-	godotenv.Load()
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("[DEBUG] No .env file found or error loading .env:", err)
+	} else {
+		fmt.Println("[DEBUG] .env file loaded successfully")
+	}
+	fmt.Println("[DEBUG] STRIPE_SECRET_KEY:", os.Getenv("STRIPE_SECRET_KEY"))
+	fmt.Println("[DEBUG] RECAPTCHA_PROJECT_ID:", os.Getenv("RECAPTCHA_PROJECT_ID"))
+	fmt.Println("[DEBUG] RECAPTCHA_API_KEY:", os.Getenv("RECAPTCHA_API_KEY"))
+	fmt.Println("[DEBUG] RECAPTCHA_SITE_KEY:", os.Getenv("RECAPTCHA_SITE_KEY"))
+	fmt.Println("[DEBUG] GOOGLE_MAPS_API_KEY:", os.Getenv("GOOGLE_MAPS_API_KEY"))
+	fmt.Println("[DEBUG] SMTP_HOST:", os.Getenv("SMTP_HOST"))
+	fmt.Println("[DEBUG] SMTP_USERNAME:", os.Getenv("SMTP_USERNAME"))
+	fmt.Println("[DEBUG] SMTP_PASSWORD:", os.Getenv("SMTP_PASSWORD"))
+	fmt.Println("[DEBUG] SMTP_FROM_EMAIL:", os.Getenv("SMTP_FROM_EMAIL"))
+	fmt.Println("[DEBUG] SMTP_TO_EMAIL:", os.Getenv("SMTP_TO_EMAIL"))
+	fmt.Println("[DEBUG] HOSPITABLE_API_KEY:", os.Getenv("HOSPITABLE_API_KEY"))
 
 	e := echo.New()
 	e.Use(middleware.Logger())
